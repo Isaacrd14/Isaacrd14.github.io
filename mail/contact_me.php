9b74b05aac72d5a7e8cc3fd7de5b31237f974f8c
isaacrd14 <isaac.riverad14@gmail.com>
2024-02-05 20:28:12 -0500

    ip

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,17 +10,19 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 
-   $name = htmlspecialchars($_POST['name']);
-   $email_address = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-   $phone = htmlspecialchars($_POST['phone']);
-   $message = htmlspecialchars($_POST['message']);
+// Sanitiza y valida los datos
+$name = htmlspecialchars(strip_tags($_POST['name']));
+$email_address = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$phone = htmlspecialchars(strip_tags($_POST['phone']));
+$message = htmlspecialchars(strip_tags($_POST['message']));
 
 // Create the email and send the message
-$to = 'jerome.lachaud@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$to = 'isaac.riverad14@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulario de contacto del sitio web: $name";
+$email_body = "Has recibido un nuevo mensaje del formulario de contacto de tu sitio web.\n\n"."Detalles:\n\nNombre: $name\nEmail: $email_address\nTeléfono: $phone\nMensaje:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
+echo "¡Mensaje enviado con éxito!";
 return true;
 ?>
